d083d615832bbba30c921abd3e7e20be99db1801
and-matt <mattioni.andrea@gmail.com>
2026-05-29 04:22:21 +0100

    transferred some code from notebook to pyscfutils

--- source/dft-lvc.py
+++ source/dft-lvc.py
@@ -1,64 +1,86 @@
 '''
+Calculate linear electron-phonon couplings using DFT in PySCF.
+Results are written in file out.h5, containing e-ph coupling matrix (cm^-1), 
+mode frequencies (cm^-1) and vectors (mass-weighted Hessian eigenvectors).
+Equilibrium DFT calculation results are stored in calc.chk.
+
 How to use this script:
-python dft-lvc-loc.py -ncpus 4 -xyz water.xyz
+python dft-lvc.py -ncpus 4 -xyz water.xyz --opt
 '''
 
-from pyscf import gto, dft, eph, lib
+
+import time
+import h5py
+import argparse
+from pyscf import gto, eph, lib
 from pyscf.geomopt.geometric_solver import optimize
-import numpy as np
-import time, h5py, argparse
+from pyscfutils import xyz_string
+
+BASIS_SET = 'cc-pvdz'
+FUNCTIONAL = 'camb3lyp'
+EPH_FUNCTIONAL = 'b3lyp'
+PCM_METHOD = 'IEF-PCM'
+SOLVENT_EPSILON = 2.38 # toluene
+CUTOFF_FREQUENCY = 80 # cm^-1
 
 
-# --- Read number of CPUs and input geometry ---
+# Read number of CPUs and input geometry
 parser = argparse.ArgumentParser()
-parser.add_argument("-ncpus", type=int, default=1, help="Number of CPU cores")
-parser.add_argument("-xyz", type=str, help="Input geometry xyz file")
+parser.add_argument('-ncpus', type=int, default=1, help='Number of CPU cores')
+parser.add_argument('-xyz', type=str, help='Input geometry xyz file')
+parser.add_argument('--opt', action='store_true', help='Optimize geometry')
 args = parser.parse_args()
 ncpus = args.ncpus
-lib.num_threads(ncpus)
 xyzfile = args.xyz
+opt = args.opt
 
 
-# --- Load molecule ---
-mol = gto.M(atom=xyzfile, basis='cc-pvdz')
+# Load molecule and set number of CPUs
+mol = gto.M(atom=xyzfile, basis=BASIS_SET)
+lib.num_threads(ncpus)
 
 
-# --- Run optimisation ---
-opt_mf = mol.RKS(xc='camb3lyp').PCM()
-opt_mf.with_solvent.method = 'IEF-PCM'
-opt_mf.with_solvent.eps = 2.38 # toluene
-start = time.time()
-mol = optimize(opt_mf)
-end = time.time()
-elapsed = end - start
-print(f'Optimize: {elapsed:.6f} seconds')
+# Optimize geometry
+if opt:
+    opt_mf = mol.RKS(xc=FUNCTIONAL).PCM()
+    opt_mf.with_solvent.method = PCM_METHOD
+    opt_mf.with_solvent.eps = SOLVENT_EPSILON
+    start = time.time()
+    mol = optimize(opt_mf)
+    end = time.time()
+    elapsed = end - start
+    print(f'Geometry optimization took {elapsed:.6f} s.')
+    # Save optimized geometry
+    _ = xyz_string(mol.elements, mol.atom_coord(unit='Angstrom'), 'opt.xyz')
 
 
-# --- Run equilibrium DFT calculation ---
-dftcalc = mol.RKS(xc='camb3lyp').PCM()
-dftcalc.with_solvent.method = 'IEF-PCM'
-dftcalc.with_solvent.eps = 2.38 # toluene
+# Calculate equilibrium electronic structure
+dftcalc = mol.RKS(xc=FUNCTIONAL).PCM()
+dftcalc.with_solvent.method = PCM_METHOD
+dftcalc.with_solvent.eps = SOLVENT_EPSILON
 dftcalc.chkfile = 'calc.chk'
 start = time.time()
 mf = dftcalc.run()
 end = time.time()
 elapsed = end - start
-print(f'DFT calc: {elapsed:.6f} seconds')
+print(f'DFT calculation took {elapsed:.6f} s.')
 
 
-# --- Run electron–phonon calculation ---
-mf.xc = 'b3lyp'
+# Calculate electron–phonon couplings
+mf.xc = EPH_FUNCTIONAL
 start = time.time()
-eph_obj = eph.EPH(mf)
+eph_obj = eph.EPH(mf, cutoff_frequency=CUTOFF_FREQUENCY, keep_imag_frequency=True)
+# electron-phonon matrix units cm^-1, shape (nmodes, nao, nao)
 ephmat, omega = eph_obj.kernel()
+# mode vectors units mass-weighted coordinates, shape (3*natm, nmodes)
 _, modevec = eph_obj.get_mode()
 end = time.time()
 elapsed = end - start
-print(f'EPH calc: {elapsed:.6f} seconds')
+print(f'EPH calculation took {elapsed:.6f} s.')
 
 
-# --- Save electron-phonon couplings and frequencies ---
-hdf5_file_path = 'dft-lvc-out.h5'
+# Save electron-phonon couplings, frequencies and mode vectors
+hdf5_file_path = 'out.h5'
 with h5py.File(hdf5_file_path, 'w') as hdf5_file:
     hdf5_file.create_dataset('ephmat',  data=ephmat)
     hdf5_file.create_dataset('omega',   data=omega)
---
+++ source/pyscfutils.py
@@ -0,0 +1,133 @@
+"""
+Functions used for pre- and post-processing of PySCF calculations.
+"""
+
+
+import time
+import h5py
+import argparse
+import os
+import numpy as np
+import scipy as sp
+import py3Dmol
+from pyscf import dft, lo, lib
+from pyscf.tools import cubegen
+
+
+def xyz_string(symbls, coords, filename=None):
+    """
+    Create xyz string from atomic symbols and coordinates.
+    Write it to file if filename is provided.
+    """
+    xyz = f"{len(symbls)}\n\n"
+    for s, (x,y,z) in zip(symbls, coords):
+        xyz += f"{s} {x} {y} {z}\n"
+    if isinstance(filename, str):
+        with open(filename, "w") as f:
+            f.write(xyz)
+    return xyz
+
+
+def mf_from_chk(chkfile):
+    """Return SCF object created from checkfile."""
+    mol = lib.chkfile.load_mol(chkfile)
+    mf = dft.RKS(mol)
+    mf.__dict__.update(lib.chkfile.load(chkfile, "scf"))
+    return mf
+
+
+def Boys_frontier_orbitals(mf, norb):
+    """
+    !!! Important: norb must be even !!!
+    Identify the frontier molecular orbitals (HOMO - norb/2 + 1, ..., LUMO + norb/2).
+    Return Boys localized MO coefficients and indices of the frontier orbitals.
+    """
+    # Identify frontier orbital indices
+    mo_occ = mf.mo_occ
+    nocc = np.count_nonzero(mo_occ > 0)
+    homo = nocc - 1
+    fomo = homo - int(norb/2) + 1 # lowest frontier OMO
+    fumo = homo + int(norb/2) + 1 # highest frontier UMO
+    frontier_idx = list(range(fomo,fumo))
+    # Localize frontier orbitals using Boys method
+    mo_coeff = mf.mo_coeff
+    mo_front = mo_coeff[:, frontier_idx]
+    mo_front_Boys = lo.Boys(mf.mol, mo_front).kernel()
+    return mo_front_Boys, frontier_idx
+
+
+def atomic_populations(mf, mo_coeff):
+    """Calculate atomic populations of molecular orbitals."""
+    natm = mf.mol.natm
+    atomic_pop = np.zeros((natm, mo_coeff.shape[1]))
+    labels = mf.mol.ao_labels()
+    for i, C in enumerate(mo_coeff.T):
+        dm_mo = np.outer(C, C)
+        pop, _ = mf.mulliken_pop(mf.mol, dm_mo, verbose=0)
+        for a in range(natm):
+            # find indices of all atomic orbitals on the same atom
+            idx = [k for k, l in enumerate(labels) if int(l.split()[0]) == a]
+            # calculate total population on an atom
+            atomic_pop[a, i] = np.sum(pop[idx])
+    return atomic_pop
+
+
+def pair_neighboring_orbitals(mf, mo_coeff):
+    """
+    !!! Important: mo_coeff.shape[1] must be even !!!
+    Calculate center of mass of molecular orbitals and reorder them so 
+    that they appear in pairs of orbitals with close center of mass,
+    i.e. on the same molecular fragment. 
+    """
+    norb = mo_coeff.shape[1]
+    atomic_pop = atomic_populations(mf, mo_coeff)
+    # centers of mass of the electronic densities
+    centers = atomic_pop.T @ mf.mol.atom_coords(unit='Angstrom')
+    # Run through each centers and find its nearest neighbor
+    remaining = list(range(norb))
+    ordered_indices = []
+    while remaining:
+        i = remaining.pop(0)
+        # distances from point i to all remaining points
+        dists = np.linalg.norm(centers[remaining] - centers[i], axis=1)
+        # nearest neighbor
+        jpos = np.argmin(dists)
+        j = remaining.pop(jpos)
+        ordered_indices.extend([i, j])
+    mo_coeff_sorted = mo_coeff.copy()[:,ordered_indices]
+    return mo_coeff_sorted, ordered_indices
+
+
+def block_frontier_orbitals(mf, mo_coeff, fock=None):
+    """
+    Block-diagonalise the Fock matrix within 2-dimensional blocks.
+    Return the localized orthogonal molecular orbital coefficients.
+    """
+    if fock is None:
+        fock = fock_matrix(mf)
+    npairs = int(mo_coeff.shape[1]/2)
+    mo_coeff_pairs, _ = pair_neighboring_orbitals(mf, mo_coeff)
+    fock_pairs = mo_coeff_pairs.T @ fock @ mo_coeff_pairs
+    # block diagonalise Fock matrix for each pair
+    mo_coeff_block = np.zeros(mo_coeff.shape)
+    u = []
+    for i in range(npairs):
+        fock_sub = fock_pairs[2*i:2*i+2, 2*i:2*i+2]
+        _, vec_sub = eighsort(fock_sub)
+        u += [vec_sub]
+    u_block = sp.linalg.block_diag(*u)
+    mo_coeff_block = mo_coeff_pairs @ u_block
+    return mo_coeff_block
+
+
+def fock_matrix(mf):
+    dm = mf.make_rdm1()
+    fock_ao = mf.get_fock(dm=dm)
+    return fock_ao
+
+
+def eighsort(mat):
+    val, vec = np.linalg.eigh(mat)
+    i = np.argsort(val)
+    return val[i], vec[:,i]
+
