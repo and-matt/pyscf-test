e251c6af9cd4a9e7a8d107d661685fd5a565bc27
and-matt <mattioni.andrea@gmail.com>
2026-06-02 14:16:09 +0100

    moved all visualization to notebook.ipynb

--- source/ephutils.py
+++ source/ephutils.py
@@ -19,26 +19,34 @@ def cartesian_modevec(modevec, mass):
     return modevec_cart
 
 
-def multiconfig_vibronic_coupling(g):
+def multiconfig_vibronic_coupling(g, which="Schroder"):
     """
-    Convert vibronic coupling from MO basis to the singlet space 
+    Convert vibronic coupling from frontier MO basis to the singlet space 
     formed by symmetric / antisymmetric singlet exciton (SE+, SE-), 
     charge-transfer (CT+, CT-), and correlated triplet-triplet (TT) states.
     Molecular orbital index ordering is assumed to be hA, lA, hB, lB
     (localized HOMO/LUMO on monomer A/B).
     Further reading:
+    - Schroder et al. (2019) https://doi.org/10.1038/s41467-019-09039-7
     - Smith & Michl (2010) https://doi.org/10.1021/cr1002613
     - Alguire et al. (2015) https://doi.org/10.1021/jp510777c
-    - Schroder et al. (2019) https://doi.org/10.1038/s41467-019-09039-7
     """
     nvib = g.shape[0]
     W = np.zeros((nvib, 5, 5))
-    W[:,0,1] = 0.5 * ( g[:,2,2] - g[:,0,0] - g[:,3,3] + g[:,1,1] )
-    W[:,2,3] = 0.5 * ( g[:,2,2] - g[:,0,0] + g[:,3,3] - g[:,1,1] )
-    W[:,0,2] = g[:,1,3] - g[:,0,3]
-    W[:,1,3] = g[:,1,3] + g[:,0,3]
-    W[:,4,2] = np.sqrt(3) / 2 * ( g[:,1,2] + g[:,0,3] )
-    W[:,4,3] = np.sqrt(3) / 2 * ( g[:,1,2] - g[:,0,3] )
+    if which=="Schroder":
+        W[:,0,1] = 0.5 * ( g[:,2,2] - g[:,0,0] - g[:,3,3] + g[:,1,1] )
+        W[:,2,3] = 0.5 * ( g[:,2,2] - g[:,0,0] + g[:,3,3] - g[:,1,1] )
+        W[:,0,2] = g[:,1,3] - g[:,0,3]
+        W[:,1,3] = g[:,1,3] + g[:,0,3]
+        W[:,4,2] = np.sqrt(3) / 2 * ( g[:,1,2] + g[:,0,3] )
+        W[:,4,3] = np.sqrt(3) / 2 * ( g[:,1,2] - g[:,0,3] )
+    elif which=="Smith":
+        W[:,2,4] = np.sqrt(3) / 2 * ( g[:,1,2] + g[:,3,0] )
+        W[:,3,4] = np.sqrt(3) / 2 * ( g[:,1,2] - g[:,3,0] )
+        W[:,2,0] = 0.5 * ( g[:,1,3] - g[:,0,2] - g[:,2,0] - g[:,3,1] )
+        W[:,2,1] = 0.5 * ( g[:,1,3] + g[:,0,2] - g[:,2,0] + g[:,3,1] )
+        W[:,3,0] = 0.5 * ( g[:,1,3] - g[:,0,2] + g[:,2,0] + g[:,3,1] )
+        W[:,3,1] = 0.5 * ( g[:,1,3] + g[:,0,2] + g[:,2,0] - g[:,3,1] )
     W = W + W.transpose(0,2,1)
     return W
 
@@ -56,7 +64,7 @@ def cluster_ephmat(data, n_clusters=3, random_state=0, plot=True):
     """K-means clustering and plotting in 2D using tSNE."""
     kmeans = KMeans(n_clusters=n_clusters, random_state=random_state, n_init=10)
     labels = kmeans.fit_predict(data)
-    # Dimensionality reduction to 2D
+    # Dimensionality reduction to 2D using t-SNE
     tsne = TSNE(n_components=2, perplexity=30, random_state=0)
     centers = kmeans.cluster_centers_
     nc = centers.shape[0]
@@ -75,3 +83,33 @@ def cluster_ephmat(data, n_clusters=3, random_state=0, plot=True):
     return kmeans
 
 
+def cluster_indices(labels):
+    """Find normal mode indices belonging to each cluster (labels=kmeans.labels_)."""
+    mode_indices = []
+    nc = labels.max() + 1
+    for c in range(nc):
+        where_cluster = np.where(labels==c)[0]
+        mode_indices += [where_cluster]
+    return mode_indices
+
+
+def lineshape(x, freq, hwhm=5):
+    """Anti-symmetrized Lorentzian, equivalent to Browninan spectral density."""
+    spec_dens = 2 * x * freq * hwhm  / np.arctan(freq / hwhm) \
+                / ((x-freq)**2 + hwhm**2) / ((x+freq)**2 + hwhm**2)
+    return spec_dens
+
+
+def effective_spectral_density(fgrid, omega, lvc, hwhm=5, plot=True):
+    """Calculate effective spectral density from LVC operators."""
+    jgrid = np.zeros(fgrid.shape)
+    for f,w in zip(omega, lvc):
+        jgrid += np.linalg.norm(w)**2 * lineshape(fgrid, f, hwhm)
+    if plot:
+        plt.figure()
+        plt.plot(fgrid, jgrid, "k-")
+        plt.xlabel(r"Frequency / cm$^{-1}$")
+        plt.ylabel(r"Spectral Density / cm$^{-1}$")
+        plt.tight_layout()
+        plt.show()
+    return jgrid
--- source/visutils.py
+++ source/visutils.py
@@ -4,6 +4,9 @@ Tools for visualizing orbitals and molecular structures.
 
 import os
 import webbrowser
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import TwoSlopeNorm
 import py3Dmol
 from dftutils import xyz_string
 from ephutils import cartesian_modevec
@@ -47,7 +50,7 @@ def view_orbital(mol_obj, cube_filename, iso=0.025, alpha=0.9, html=True):
     return view
 
 
-def view_vibration(mol_obj, modevec, amplitude=0.5, equilibrium=False):
+def view_vibration_distorted(mol_obj, modevec, amplitude=0.5, equilibrium=False):
     """
     Visualize distorted geometries along a vibrational mode.
     Generate view with py3Dmol which can be displayed as view.show().
@@ -79,4 +82,87 @@ def view_vibration(mol_obj, modevec, amplitude=0.5, equilibrium=False):
     view.rotate(-45, "x")
     view.zoom(1.5)
     return view
+
+
+def view_vibration_animation(mol_obj, modevec, amplitude=0.5, period=1000, fps=60):
+    """
+    Visualize vibrational animation along a vibrational mode.
+    Generate view with py3Dmol which can be displayed as view.show().
+    """
+    view = py3Dmol.view(width=600, height=300)
+    symbols = mol_obj.elements
+    coords = mol_obj.atom_coords(unit="Angstrom")
+    mass = mol_obj.atom_mass_list()
+    displacement = amplitude * cartesian_modevec(modevec, mass)
+    xyz_frames = ""
+    # calculate number of frames from period (ms) and fps
+    nframes = round(period / 1000 * fps)
+    times = np.linspace(0, 2*np.pi, nframes+1)[:-1]
+    for t in times:
+        coords_distorted = coords + np.sin(t) * displacement
+        xyz_frames += xyz_string(symbols, coords_distorted, filename=False)
+    view.addModelsAsFrames(xyz_frames, "xyz")
+    view.animate({"loop" : "forward", 
+                  "reps" : 0,
+                  "interval" : period / nframes})
+    view.setStyle({"stick":{"colorscheme":"grayCarbon"}})
+    view.setViewStyle({"style": "outline"})
+    view.setBackgroundColor("white")
+    view.zoomTo()
+    view.rotate(-45, "x")
+    view.zoom(1.5)
+    return view
+
+
+def view_vibration_arrows(mol_obj, modevec, amplitude=5.0, threshold=0.5):
+    """
+    Visualize distortion along a vibrational mode using arrows.
+    Generate view with py3Dmol which can be displayed as view.show().
+    """
+    view = py3Dmol.view(width=600, height=300)
+    symbols = mol_obj.elements
+    coords = mol_obj.atom_coords(unit="Angstrom")
+    view.addModel(xyz_string(symbols, coords), "xyz")
+    mass = mol_obj.atom_mass_list()
+    displacement = amplitude * cartesian_modevec(modevec, mass)
+    for r, dr in zip(coords, displacement):
+        if np.linalg.norm(dr) > threshold:
+            start = {"x": float(r[0]),
+                     "y": float(r[1]),
+                     "z": float(r[2])}
+            end = {"x": float(r[0] + dr[0]),
+                   "y": float(r[1] + dr[1]),
+                   "z": float(r[2] + dr[2])}
+            view.addArrow({"start": start,
+                           "end": end,
+                           "radius": 0.1,
+                           "color": "red"})
+    view.setStyle({"stick":{"colorscheme":"grayCarbon"}})
+    view.setViewStyle({"style": "outline"})
+    view.setBackgroundColor("white")
+    view.zoomTo()
+    view.rotate(-45, "x")
+    view.zoom(1.5)
+    return view
+
+
+def view_multiconfig_vibronic_coupling(mat, frequency=None, coupling=None):
+    """Plot e-ph coupling matrix in the singlet multiconfigurational subspace."""
+    state_labels = [r'SE$_+$', r'SE$_-$', r'CT$_+$', r'CT$_-$', r'TT']
+    # Set range for diverging colormap
+    vmin = min(mat.min(), 0)
+    vmax = max(mat.max(), 0)
+    v = max(np.abs(vmin), np.abs(vmax))
+    norm = TwoSlopeNorm(vmin=-v, vcenter=0, vmax=+v)
+    # Plot
+    fig, ax = plt.subplots()
+    p = ax.imshow(mat, cmap='bwr', norm=norm)
+    fig.colorbar(p, ax=ax)
+    ax.set_xticks(ticks=list(range(5)), labels=state_labels)
+    ax.set_yticks(ticks=list(range(5)), labels=state_labels)
+    if (frequency is not None) and (coupling is not None):
+        f, c = round(frequency), round(coupling)
+        ax.set_title(fr"$\omega$ = {f} cm$^{{-1}}$, $w$ = {c} cm$^{{-1}}$")
+    return fig
+
 
